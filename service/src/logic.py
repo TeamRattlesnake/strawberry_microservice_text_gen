5f290323efdb8805c6e77854bef63f091d3a29a5
Андрей Коленков <avoknelok@mail.ru>
2023-03-24 16:35:53 +0300

    Изменил имя логов

--- service/src/logic.py
+++ service/src/logic.py
@@ -18,7 +18,7 @@ from transformers import Trainer, TrainingArguments
 
 
 logging.basicConfig(format="%(asctime)s %(message)s", handlers=[logging.FileHandler(
-    f"/home/logs/log-{datetime.datetime.now()}.txt", mode="a", encoding="UTF-8")], datefmt="%I:%M:%S %p", level=logging.INFO)
+    f"/home/logs/text_gen_{datetime.datetime.now()}.txt", mode="a", encoding="UTF-8")], datefmt="%I:%M:%S %p", level=logging.INFO)
 
 
 class NeuralNetwork:
